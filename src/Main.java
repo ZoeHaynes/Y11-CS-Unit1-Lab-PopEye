7d0f5b5b35493d066fa8778263ab6c1db783b004
ZoeHaynes <48744568+ZoeHaynes@users.noreply.github.com>
2023-02-02 14:21:34 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the distance to travel in nautical miles: ");
+        System.out.print("Enter the distance to travel in nautical miles: ");
         double nauticalMiles = scanner.nextDouble();
         double regularMiles = nauticalMiles*1.1508;
-        System.out.println("Enter the top speed of the boat in knots: ");
+        System.out.print("Enter the top speed of the boat in knots: ");
         double topSpeed = scanner.nextDouble();
         double bestCase = nauticalMiles/topSpeed;
-        System.out.println("Enter the top speed of the boat in knots: ");
+        System.out.print("Enter the top speed of the boat in knots: ");
         double aveSpeed = scanner.nextDouble();
         double aveCase = nauticalMiles/aveSpeed;
-        System.out.println("The distance to travel in regular miles is: " + regularMiles + " miles");
-        System.out.println("The best case travel time is: " + bestCase + " hours at " + topSpeed + " knots");
-        System.out.println("The average case travel time is: " + aveCase + " hours at " + aveSpeed + " knots");
+        System.out.println("\nThe distance to travel in regular miles is: " + regularMiles + " miles\nThe best case travel time is: " + bestCase + " hours at " + topSpeed + " knots\nThe average case travel time is: " + aveCase + " hours at " + aveSpeed + " knots");
 
 
 
